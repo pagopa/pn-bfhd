fb63c2a79116b986277d6c7fea1e6a7091973b3f
lele-OFA <emanuele.didonato@pagopa.it>
2026-02-23 16:29:28 +0100

    feature: finish lambda

--- functions/pn-notificationWrapper/src/app/eventHandler.js
+++ functions/pn-notificationWrapper/src/app/eventHandler.js
@@ -2,29 +2,29 @@ const axios = require('axios');
 
 async function handleEvent(event, context) {
 
+
+        //const logPath = '/bfhd/logs/v1/notifications/info';
+        const path = "/delivery/v2.8/notifications/sent/";
+        const iun = event.pathParameters["iun"];
+        const url = `http://${process.env.APPLICATION_LOAD_BALANCER_DOMAIN}:8080${path}${iun}`;
+        const headers = JSON.parse(JSON.stringify(event["headers"]));
+        if (event.requestContext.authorizer["cx_id"]) {
+         headers["x-pagopa-pn-cx-id"] = event.requestContext.authorizer["cx_id"];
+        }
+        if (event.requestContext.authorizer["cx_type"]) {
+         headers["x-pagopa-pn-cx-type"] = event.requestContext.authorizer["cx_type"];
+        }
+        if (event.requestContext.authorizer["uid"]) {
+            headers["x-pagopa-pn-uid"] = event.requestContext.authorizer["uid"];
+        }
     try {
-        const body = typeof event.body === 'string' ? JSON.parse(event.body) : event.body;
-        const iun = body?.iun || body?.iuns?.[0] || event.iun;
-    } catch (e) {
-        console.log(e);
-    }
-
-    // bisogna definire questo parametro su aws, nella lambda, come parametro di impostazione in base all ambiente.
-    //definirlo poi dentro microservice.yml e utilizzarlo qui
-    const host = process.env.BFHD_API_HOST || 'https://api.bo.uat.notifichedigitali.it';
-    const logPath = '/bfhd/logs/v1/notifications/info';
-    const path = "/notifications/sent/";
-    const IUN = event.pathParameters["iun"];
-    const url = `${process.env.PN_DELIVERY_URL}${path}${IUN}`;
-
-   //Token deve essere preso dagli headers
-    //const token = process.env.AUTH_TOKEN ? process.env.AUTH_TOKEN.replace('Bearer ', '') : '';
-
-    try {
-        const apiResponse = await axios.post(url,
+        const apiResponse = await axios.get(url,
         {
-            iuns: iun ? [iun] : [],
-            ticketNumber: "SNDA-1000"
+         headers: {
+        "x-pagopa-pn-uid":headers["x-pagopa-pn-cx-id"],
+        "x-pagopa-pn-cx-type":headers["x-pagopa-pn-cx-type"],
+        "x-pagopa-pn-cx-id":headers["x-pagopa-pn-uid"]
+        }
         },
         {});
 
