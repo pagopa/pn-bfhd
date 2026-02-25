07412d52cca07c1a5a06aa80b36050eed59ddb06
lele-OFA <emanuele.didonato@pagopa.it>
2026-02-25 10:23:17 +0100

    fix: delete comment

--- functions/pn-notificationWrapper/src/app/eventHandler.js
+++ functions/pn-notificationWrapper/src/app/eventHandler.js
@@ -3,30 +3,29 @@ const axios = require('axios');
 async function handleEvent(event, context) {
 
 
-        //const logPath = '/bfhd/logs/v1/notifications/info';
-        const path = "/delivery/v2.8/notifications/sent/";
-        const iun = event.pathParameters["iun"];
-        const url = `http://${process.env.APPLICATION_LOAD_BALANCER_DOMAIN}:8080${path}${iun}`;
-        const headers = JSON.parse(JSON.stringify(event["headers"]));
-        if (event.requestContext.authorizer["cx_id"]) {
-         headers["x-pagopa-pn-cx-id"] = event.requestContext.authorizer["cx_id"];
-        }
-        if (event.requestContext.authorizer["cx_type"]) {
-         headers["x-pagopa-pn-cx-type"] = event.requestContext.authorizer["cx_type"];
-        }
-        if (event.requestContext.authorizer["uid"]) {
-            headers["x-pagopa-pn-uid"] = event.requestContext.authorizer["uid"];
-        }
+    const path = "/delivery/v2.8/notifications/sent/";
+    const iun = event.pathParameters["iun"];
+    const url = `http://${process.env.APPLICATION_LOAD_BALANCER_DOMAIN}:8080${path}${iun}`;
+    const headers = JSON.parse(JSON.stringify(event["headers"]));
+    if (event.requestContext.authorizer["cx_id"]) {
+        headers["x-pagopa-pn-cx-id"] = event.requestContext.authorizer["cx_id"];
+    }
+    if (event.requestContext.authorizer["cx_type"]) {
+        headers["x-pagopa-pn-cx-type"] = event.requestContext.authorizer["cx_type"];
+    }
+    if (event.requestContext.authorizer["uid"]) {
+        headers["x-pagopa-pn-uid"] = event.requestContext.authorizer["uid"];
+    }
     try {
         const apiResponse = await axios.get(url,
-        {
-         headers: {
-        "x-pagopa-pn-uid":headers["x-pagopa-pn-cx-id"],
-        "x-pagopa-pn-cx-type":headers["x-pagopa-pn-cx-type"],
-        "x-pagopa-pn-cx-id":headers["x-pagopa-pn-uid"]
-        }
-        },
-        {});
+            {
+                headers: {
+                    "x-pagopa-pn-uid": headers["x-pagopa-pn-cx-id"],
+                    "x-pagopa-pn-cx-type": headers["x-pagopa-pn-cx-type"],
+                    "x-pagopa-pn-cx-id": headers["x-pagopa-pn-uid"]
+                }
+            },
+            {});
 
         return {
             statusCode: 200,
